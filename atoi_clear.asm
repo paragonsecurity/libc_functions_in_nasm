0327ff55d9cb196828503d9958c290b51338fadb
Paragon Security <51992254+paragonsecurity@users.noreply.github.com>
2019-06-19 11:40:24 +0200

    Update atoi_clear.asm

--- atoi_clear.asm
+++ atoi_clear.asm
@@ -128,3 +128,30 @@ Bad_format:
 ;    syscall
     mov rax, 1
     jmp _ret
+    
+ 
+;==================================
+
+; Réimplémentation des fonctions de la libc en nasm 64 bits
+; Une fonction qui calcule la taille d'une chaine de caractère en octets
+
+;=================================
+ 
+ 
+_strlen:
+    push rbp
+    mov rbp, rsp
+    mov rcx, 0
+    mov rax, rdi
+    jmp _loop
+
+_loop:
+    cmp byte [rax], 0
+    je _end
+    inc rax
+    inc rcx
+    jmp _loop
+
+_end:
+    mov rax, rcx
+    jmp _ret
