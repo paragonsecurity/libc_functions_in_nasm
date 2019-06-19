bf04e0ec1acceb7c8cdcfe753d69fa0e05cf8136
Paragon Security <51992254+paragonsecurity@users.noreply.github.com>
2019-06-19 09:46:50 +0000

    Add files via upload

---
+++ strlen.asm
@@ -0,0 +1,30 @@
+
+
+;==================================
+
+; Réimplémentation des fonctions de la libc en nasm 64 bits
+; Une fonction qui calcule la taille d'une chaine de caractère en octets
+
+;=================================
+
+_strlen:
+    push rbp
+    mov rbp, rsp
+    mov rcx, 0
+    mov rax, rdi
+    jmp _loop
+
+_loop:
+    cmp byte [rax], 0
+    je _end
+    inc rax
+    inc rcx
+    jmp _loop
+
+_end:
+    mov rax, rcx
+    jmp _ret
+
+_ret:
+    leave ; mov rsp, rbp ; pop rbp
+    ret ; pop rip && jmp rip
